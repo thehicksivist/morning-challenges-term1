60c249bfa7a1bed978786a5996697cc0f28f3132
thehicksivist <hicks.a.james@gmail.com>
2018-09-18 09:13:18 +1000

    sync master

--- 12_count_letters.rb
+++ 12_count_letters.rb
@@ -15,22 +15,15 @@
 #
 
 def count_letters (string)
-  result = {} # You'll need an empty hash to get started!
-  string.chars.each do |x|
-    # add each new char to hash as key
-    # count each char
-    if result.key? x
-      puts "x in result"
-    else
-      puts "nah"
-    end
+   result = Hash.new 0# You'll need an empty hash to get started!
+  string.chars.each do |x|   # add each new char to hash as key
+  result[x] += 1
   end
-      
-
-  result # return the hash
+  list = result.keys
+  return list
+  puts "#{result}"
+ puts "#{list}"
 end
-
-# {|a,c| a[c] += 1; a}
 
 count_letters("hello") 
 # should return {"h"=>1, "e"=>1, "l"=>2, o=>1}
