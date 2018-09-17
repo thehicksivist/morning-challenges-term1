72bb0bdb2ee09672bd4a4b2f4666a932104923f5
thehicksivist <hicks.a.james@gmail.com>
2018-09-17 21:14:17 +1000

    16 complete

--- 16_unique_values.rb
+++ 16_unique_values.rb
@@ -19,5 +19,42 @@
 #
 
 def unique (mylist)
-  # Your code here
+  arr = [] 
+  mylist.each do |x|
+    arr << x
+  end
+  counts = Hash.new 0
+  arr.each do |i|
+  counts[i] += 1
+  end
+  list = counts.keys
+  return list
 end
+
+unique([1,2,3,3]) 
+#should return [1,2,3]
+unique(["tom", "tom", "tom"]) 
+# should return ["tom"]
+
+#FIRST SOLUTION: program works, but doesn't pass test
+# def unique (mylist)
+#   sicklist = []
+#   # Your code here
+#   for x in mylist
+#     sicklist << x
+#   end
+#   for i in sicklist
+#     if sicklist.count(i) > 1
+#       sicklist.delete(i)
+#       sicklist << i
+#     else
+#     end
+#   end
+#   return sicklist
+#   puts "#{sicklist}"
+# end
+
+# unique([1,2,3,3]) 
+# # should return [1,2,3]
+# unique(["tom", "tom", "tom"]) 
+# # should return ["tom"]
